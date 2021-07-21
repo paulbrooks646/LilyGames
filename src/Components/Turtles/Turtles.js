bac555082bf1f930acb2442e50bd6af8bb35dcf7
paulbrooks646 <paulbrooks646@gmail.com>
2021-07-21 13:39:37 -0600

    turtles cribs

--- src/Components/Turtles/Turtles.js
+++ src/Components/Turtles/Turtles.js
@@ -5,7 +5,88 @@ export default function Turtles() {
     return (
         <div className="main">
             <nav className="turtles-nav"></nav>
-            <House/>
+            <House />
+            <div className="turtles-top">
+                <div className="turtles-crib">
+                    <div className="turtles-crib-michelangelo">
+                        <h3>Michelangelo</h3>
+                    </div>
+                    <div className="turtles-crib-middle">
+                        <div className="turtles-crib-bar"></div>
+                        <div className="turtles-crib-bar"></div>
+                        <div className="turtles-crib-bar"></div>
+                        <div className="turtles-crib-bar"></div>
+                        <div className="turtles-crib-bar"></div>
+                        <div className="turtles-crib-bar"></div>
+                    </div>
+                    <div className="turtles-crib-bottom"></div>
+                    <div className="turtles-crib-legs">
+                        <div className="turtles-crib-leg"></div>
+                        <div className="turtles-crib-leg"></div>
+                    </div>
+                </div>
+                <div className="turtles-crib">
+                    <div className="turtles-crib-donatello">
+                        <h3>Donatello</h3>
+                    </div>
+                    <div className="turtles-crib-middle">
+                        <div className="turtles-crib-bar"></div>
+                        <div className="turtles-crib-bar"></div>
+                        <div className="turtles-crib-bar"></div>
+                        <div className="turtles-crib-bar"></div>
+                        <div className="turtles-crib-bar"></div>
+                        <div className="turtles-crib-bar"></div>
+                    </div>
+                    <div className="turtles-crib-bottom"></div>
+                    <div className="turtles-crib-legs">
+                        <div className="turtles-crib-leg"></div>
+                        <div className="turtles-crib-leg"></div>
+                    </div>
+                </div>
+                <div className="turtles-crib">
+                    <div className="turtles-crib-leonardo"><h3>Leonardo</h3></div>
+                    <div className="turtles-crib-middle">
+                        <div className="turtles-crib-bar"></div>
+                        <div className="turtles-crib-bar"></div>
+                        <div className="turtles-crib-bar"></div>
+                        <div className="turtles-crib-bar"></div>
+                        <div className="turtles-crib-bar"></div>
+                        <div className="turtles-crib-bar"></div>
+                    </div>
+                    <div className="turtles-crib-bottom"></div>
+                    <div className="turtles-crib-legs">
+                        <div className="turtles-crib-leg"></div>
+                        <div className="turtles-crib-leg"></div>
+                    </div>
+                </div>
+                <div className="turtles-crib">
+                    <div className="turtles-crib-raphael"><h3>Raphael</h3></div>
+                    <div className="turtles-crib-middle">
+                        <div className="turtles-crib-bar"></div>
+                        <div className="turtles-crib-bar"></div>
+                        <div className="turtles-crib-bar"></div>
+                        <div className="turtles-crib-bar"></div>
+                        <div className="turtles-crib-bar"></div>
+                        <div className="turtles-crib-bar"></div>
+                    </div>
+                    <div className="turtles-crib-bottom"></div>
+                    <div className="turtles-crib-legs">
+                        <div className="turtles-crib-leg"></div>
+                        <div className="turtles-crib-leg"></div>
+                    </div>
+                </div>
+            </div>
+            <div className="turtles-middle">
+                <div className="turtles-middle-left"></div>
+                <div className="turtles-middle-middle"></div>
+                <div className="turtles-middle-right"></div>
+            </div>
+            <div className="turtles-bottom">
+                <div className="turtles-bottom-left"></div>
+                <div className="turtles-bottom-middle"></div>
+                <div className="turtles-bottom-right"></div>
+            </div>
+
         </div>
     )
 }
