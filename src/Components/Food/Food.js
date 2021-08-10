da128e497a312f25fb5e92e13762b9ce50d4d023
paulbrooks646 <paulbrooks646@gmail.com>
2021-08-10 11:59:36 -0600

    cookie done

--- src/Components/Food/Food.js
+++ src/Components/Food/Food.js
@@ -11,162 +11,221 @@ export default function Food() {
     <div className="main">
       <nav className="food-nav"></nav>
       <House />
-      <div className="food-top-left"></div>
+      <div className="food-top">
+        {/* <div className="food-top-left"></div>
       <div className="food-top-middle"></div>
-      <div className="food-top-right"></div>
-      <div className="food-middle-left">
-        <div className="bone-div">
-          <div className="bone">
-            <div className="bone-left">
-              <div className="bone-top-corner"></div>
-              <div className="bone-bottom-corner"></div>
-            </div>
-            <div className="bone-middle"></div>
-            <div className="bone-right">
-              <div className="bone-top-corner"></div>
-              <div className="bone-bottom-corner"></div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="food-middle-middle">
-        <div className="pizza-div">
-          <div className="pizza">
-            <div className="pizza-horizontal-line"></div>
-            <div className="pizza-vertical-line"></div>
-            <div className="pepperoni-outer">
-              <div className="pepperoni"></div>
-              <div className="pepperoni"></div>
-              <div className="pepperoni"></div>
-            </div>
-            <div className="pepperoni-innner">
-              <div className="pepperoni"></div>
-              <div className="pepperoni"></div>
-              <div className="pepperoni"></div>
-              <div className="pepperoni"></div>
-            </div>
-            <div className="pepperoni-innner">
-              <div className="pepperoni"></div>
-              <div className="pepperoni"></div>
-              <div className="pepperoni"></div>
-              <div className="pepperoni"></div>
-            </div>
-            <div className="pepperoni-outer">
-              <div className="pepperoni"></div>
-              <div className="pepperoni"></div>
-              <div className="pepperoni"></div>
-            </div>
-          </div>
-          <div className="pizza-slice"></div>
-        </div>
-      </div>
-      <div className="food-middle-right">
-        <div className="corn-div">
-          <div className="corn">
-            <div className="corn-line"></div>
-            <div className="corn-line"></div>
-            <div className="corn-line"></div>
-            <div className="corn-line"></div>
-            <div className="corn-line"></div>
-          </div>
-        </div>
-      </div>
-      <div className="food-bottom-left">
-        <div className="bowl-div">
-          <div className="bowl">
-            <div className="inner-bowl">
-              <div className="bowl-row-one">
-                <div className="mnm-red">
-                  <p className="mnmm">m</p>
-                </div>
-                <div className="mnm-yellow">
-                  <p className="mnmm">m</p>
-                </div>
-                <div className="mnm-blue">
-                  <p className="mnmm">m</p>
+        <div className="food-top-right"></div> */}
+        <div className="food-shelf">
+          <div className="food-shelf-left"></div>
+          <div className="food-shelf-middle">
+            <div className="food-shelf-board"></div>
+            <div className="food-shelf-space">
+              <div className="bone-div">
+                <div className="bone">
+                  <div className="bone-left">
+                    <div className="bone-top-corner"></div>
+                    <div className="bone-bottom-corner"></div>
+                  </div>
+                  <div className="bone-middle"></div>
+                  <div className="bone-right">
+                    <div className="bone-top-corner"></div>
+                    <div className="bone-bottom-corner"></div>
+                  </div>
                 </div>
               </div>
-              <div className="bowl-row-two">
-                <div className="mnm-green">
-                  <p className="mnmm">m</p>
+              <div className="pizza-div">
+                <div className="pizza">
+                  <div className="pizza-horizontal-line"></div>
+                  <div className="pizza-vertical-line"></div>
+                  <div className="pepperoni-outer">
+                    <div className="pepperoni"></div>
+                    <div className="pepperoni"></div>
+                    <div className="pepperoni"></div>
+                  </div>
+                  <div className="pepperoni-innner">
+                    <div className="pepperoni"></div>
+                    <div className="pepperoni"></div>
+                    <div className="pepperoni"></div>
+                    <div className="pepperoni"></div>
+                  </div>
+                  <div className="pepperoni-innner">
+                    <div className="pepperoni"></div>
+                    <div className="pepperoni"></div>
+                    <div className="pepperoni"></div>
+                    <div className="pepperoni"></div>
+                  </div>
+                  <div className="pepperoni-outer">
+                    <div className="pepperoni"></div>
+                    <div className="pepperoni"></div>
+                    <div className="pepperoni"></div>
+                  </div>
                 </div>
-                <div className="mnm-orange">
-                  <p className="mnmm">m</p>
-                </div>
-                <div className="mnm-brown">
-                  <p className="mnmm">m</p>
-                </div>
-                <div className="mnm-red">
-                  <p className="mnmm">m</p>
-                </div>
+                <div className="pizza-slice"></div>
               </div>
-              <div className="bowl-row-three">
-                <div className="mnm-yellow">
-                  <p className="mnmm">m</p>
-                </div>
-                <div className="mnm-blue">
-                  <p className="mnmm">m</p>
-                </div>
-                <div className="mnm-green">
-                  <p className="mnmm">m</p>
-                </div>
-                <div className="mnm-orange">
-                  <p className="mnmm">m</p>
-                </div>
-                <div className="mnm-brown">
-                  <p className="mnmm">m</p>
-                </div>
-              </div>
-              <div className="bowl-row-four">
-                <div className="mnm-red">
-                  <p className="mnmm">m</p>
-                </div>
-                <div className="mnm-yellow">
-                  <p className="mnmm">m</p>
-                </div>
-                <div className="mnm-blue">
-                  <p className="mnmm">m</p>
-                </div>
-                <div className="mnm-green">
-                  <p className="mnmm">m</p>
-                </div>
-              </div>
-              <div className="bowl-row-five">
-                <div className="mnm-orange">
-                  <p className="mnmm">m</p>
-                </div>
-                <div className="mnm-brown">
-                  <p className="mnmm">m</p>
-                </div>
-                <div className="mnm-red">
-                  <p className="mnmm">m</p>
+            </div>
+            <div className="food-shelf-board"></div>
+            <div className="food-shelf-space">
+              <div className="corn-div">
+                <div className="corn">
+                  <div className="corn-line"></div>
+                  <div className="corn-line"></div>
+                  <div className="corn-line"></div>
+                  <div className="corn-line"></div>
+                  <div className="corn-line"></div>
                 </div>
               </div>
             </div>
+            <div className="food-shelf-board"></div>
+            <div className="food-shelf-space">
+              <div className="bowl-div">
+                <div className="bowl">
+                  <div className="inner-bowl">
+                    <div className="bowl-row-one">
+                      <div className="mnm-red">
+                        <p className="mnmm">m</p>
+                      </div>
+                      <div className="mnm-yellow">
+                        <p className="mnmm">m</p>
+                      </div>
+                      <div className="mnm-blue">
+                        <p className="mnmm">m</p>
+                      </div>
+                    </div>
+                    <div className="bowl-row-two">
+                      <div className="mnm-green">
+                        <p className="mnmm">m</p>
+                      </div>
+                      <div className="mnm-orange">
+                        <p className="mnmm">m</p>
+                      </div>
+                      <div className="mnm-brown">
+                        <p className="mnmm">m</p>
+                      </div>
+                      <div className="mnm-red">
+                        <p className="mnmm">m</p>
+                      </div>
+                    </div>
+                    <div className="bowl-row-three">
+                      <div className="mnm-yellow">
+                        <p className="mnmm">m</p>
+                      </div>
+                      <div className="mnm-blue">
+                        <p className="mnmm">m</p>
+                      </div>
+                      <div className="mnm-green">
+                        <p className="mnmm">m</p>
+                      </div>
+                      <div className="mnm-orange">
+                        <p className="mnmm">m</p>
+                      </div>
+                      <div className="mnm-brown">
+                        <p className="mnmm">m</p>
+                      </div>
+                    </div>
+                    <div className="bowl-row-four">
+                      <div className="mnm-red">
+                        <p className="mnmm">m</p>
+                      </div>
+                      <div className="mnm-yellow">
+                        <p className="mnmm">m</p>
+                      </div>
+                      <div className="mnm-blue">
+                        <p className="mnmm">m</p>
+                      </div>
+                      <div className="mnm-green">
+                        <p className="mnmm">m</p>
+                      </div>
+                    </div>
+                    <div className="bowl-row-five">
+                      <div className="mnm-orange">
+                        <p className="mnmm">m</p>
+                      </div>
+                      <div className="mnm-brown">
+                        <p className="mnmm">m</p>
+                      </div>
+                      <div className="mnm-red">
+                        <p className="mnmm">m</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="nut-div">
+                <div className="nut">
+                  <div className="nut-stem"></div>
+                  <div className="nut-top"></div>
+                  <div className="nut-bottom"></div>
+                </div>
+              </div>
+            </div>
+            <div className="food-shelf-board"></div>
+          </div>
+          <div className="food-shelf-right"></div>
+        </div>
+        <div className="lily-sign-div">
+          <div className="lily-sign">
+            <Typography variant="h4" className="food-sign">
+              Lily's Store
+            </Typography>
           </div>
         </div>
-      </div>
-      <div className="food-bottom-middle">
-        <div className="nut-div">
-          <div className="nut">
-            <div className="nut-stem"></div>
-            <div className="nut-top"></div>
-            <div className="nut-bottom"></div>
-          </div>
-        </div>
-      </div>
-      <div className="food-bottom-right">
+        <div className="food-shelf">
+          <div className="food-shelf-left"></div>
+          <div className="food-shelf-middle">
+            <div className="food-shelf-board"></div>
+            <div className="food-shelf-space">
               <div className="banana-div">
-                  <div className="banana-top"></div>
-                  <div className="bananas">
-                      <div className="banana-line"></div>
-            <div className="banana-one"></div>
-            <div className="banana-two"></div>
-            <div className="banana-three"></div>
-            <div className="banana-four"></div>
+                <div className="banana-top"></div>
+                <div className="bananas">
+                  <div className="banana-one"></div>
+                  <div className="banana-two"></div>
+                  <div className="banana-three"></div>
+                  <div className="banana-four"></div>
+                </div>
+              </div>
+              <div className="cheese-div">
+                <div className="cheese">
+                  <div className="cheese-top">
+                    <div className="cheese-hole"></div>
+                  </div>
+                  <div className="cheese-bottom">
+                    <div className="cheese-hole"></div>
+                    <div className="cheese-hole-two"></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="food-shelf-board"></div>
+            <div className="food-shelf-space">
+              <div className="cookie-div">
+                <div className="cookie">
+                  <div className="cookie-top">
+                    <div className="chocolate-chip"></div>
+                    <div className="chocolate-chip"></div>
+                  </div>
+                  <div className="cookie-middle">
+                    <div className="chocolate-chip"></div>
+                  </div>
+                  <div className="cookie-bottom">
+                    <div className="chocolate-chip"></div>
+                    <div className="chocolate-chip"></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="food-shelf-board"></div>
+            <div className="food-shelf-space"></div>
+            <div className="food-shelf-board"></div>
           </div>
+          <div className="food-shelf-right"></div>
         </div>
       </div>
+      <div className="food-middle-left"></div>
+      <div className="food-middle-middle"></div>
+      <div className="food-middle-right"></div>
+      <div className="food-bottom-left"></div>
+      <div className="food-bottom-middle"></div>
+      <div className="food-bottom-right"></div>
     </div>
   );
 }
